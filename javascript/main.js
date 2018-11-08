79bf457fffc56661e4e16791eed1b5c8e92b6ab0
b3rg3t <berget_90db@hotmail.com>
2018-11-08 12:56:25 +0100

    modal

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById('landingpage');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+  else if (document.getElementsByClassName('show')) {
+  }
+}
