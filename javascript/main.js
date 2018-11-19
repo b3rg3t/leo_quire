f156fd0d9484a880fd0fc7add55125c04aa86ef1
Jesper Risell <risell.jesper@gmail.com>
2018-11-19 14:44:38 +0100

    added separate saves, added new code for titles

--- javascript/main.js
+++ javascript/main.js
@@ -48,21 +48,50 @@ window.onload = function () {
   cookieCheck();
   updateView();
 };
+
+function setActiveId(id){
+  sessionStorage.activeId = id;
+  console.log("Active set to " + sessionStorage.activeId);
+}
+
+function getActiveId(){
+  return Number(sessionStorage.activeId);
+}
+
 // Loads a specific note from local storage to the editor 
 function loadToQuill(id) {
   let tempStorage = loadNotes();
   quill.setContents(tempStorage.find(loadNote => loadNote.id == id).content);
+  setActiveId(id);
+  document.getElementById("title").value = tempStorage.find(loadNote => loadNote.id == id).title;
 }
 
 function addNote() {
   let tempStorage = loadNotes(); // Loads array from loadNotes()
-  tempStorage.push(newNote(getAvailID(tempStorage))); // Adds a new note at the end of the array. 
+  let freeID = getAvailID(tempStorage);
+  setActiveId(freeID);
+  
+  tempStorage.push(newNote(freeID)); // Adds a new note at the end of the array. 
   saveNotes(tempStorage); // Overwrites localStorage with tempStorage content
+  loadToQuill(getActiveId());
+  quill.setContents("");
+  
+  saveNote();
   updateView();
   console.log(loadNotes());
 }
 // Adds and checks the length of an array. If the length is over 0 - find the highest id. Return id +1.   
 const getAvailID = noteArray => noteArray.length > 0 ? Math.max(...noteArray.map(note => note.id), 0) + 1 : 0;
+
+function saveNote() {
+  // console.log("saveNote was called.");
+  let id = getActiveId();
+  let tempStorage = loadNotes();
+  tempStorage.find(loadNote => loadNote.id == id).content = quill.getContents();
+  tempStorage.find(loadNote => loadNote.id == id).title = document.getElementById("title").value;
+  saveNotes(tempStorage);
+  updateView();
+}
 
 // Converts JSON to string and saves to localStorage.
 function saveNotes(notes) {
@@ -82,8 +111,8 @@ function deleteNote(id) {
 
 function newNote(id) {
   let note = {};
-  note.title = "Note" + " " + (1 + id); // Writes note + id starting from 1 and adds +1 for every note.
-  note.content = quill.getContents()
+  note.title = "Untitled " + (id + 1); // Writes note + id starting from 1 and adds +1 for every note.
+  note.content = quill.getContents();
   note.id = id;
   return note;
 }
